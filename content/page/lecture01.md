36c944f254656f7a7edf82f0a0b31881316fd732
Noboru Murata <noboru.murata@gmail.com>
2021-04-09 21:31:54 +0900

    debug

--- content/page/lecture01.md
+++ content/page/lecture01.md
@@ -6,7 +6,7 @@ draft = false
 weight = 1002
 +++
 
-<span class="timestamp-wrapper"><span class="timestamp">&lt;2021-04-06 Tue&gt;</span></span>
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2021-04-09 Fri&gt; </span></span> 更新
 
 
 ## スライド {#スライド}
--- docs/code/slide01.R
+++ docs/code/slide01.R
@@ -9,14 +9,23 @@ log(exp(2)+1) # 適当な数学関数の例
 ### 練習問題 データフレームの取り扱い
 ## データフレームの作成
 df <- data.frame(
+    name=c("Alice","Bob","Cathy","David"),
     literature=c(90,80,70,60),
     math=c(25,50,75,100),
     english=c(65,100,70,40))
 ## 日本語文字を使うことも可能だが，設定により文字化けが起こるので注意
-row.names(df) <- c("Alice","Bob","Cathy","David")
 df # または Environment タグから click する
 
 ## データフレームの操作
 df[,"math"]
-df["Cathy",]
-## データフレームの一部を取り出す操作を試してみよう
+df[3,]
+
+## name列の代わりに行に名前を付けることもできる
+df2 <- data.frame(
+    literature=c(90,80,70,60),
+    math=c(25,50,75,100),
+    english=c(65,100,70,40))
+row.names(df2) <- c("Alice","Bob","Cathy","David")
+df2
+df2[,"math"]
+df2["Cathy",] # 行の名前で参照可能
