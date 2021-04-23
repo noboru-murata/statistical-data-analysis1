c6400a3e4a7b1cae829c8ffb38484866aab533d1
Noboru Murata <noboru.murata@gmail.com>
2021-04-23 16:15:40 +0900

    2021 version

--- content/page/lecture04.md
+++ content/page/lecture04.md
@@ -6,7 +6,7 @@ draft = false
 weight = 1005
 +++
 
-昨年度資料
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2021-04-23 Fri&gt; </span></span> 更新
 
 
 ## スライド {#スライド}
@@ -22,3 +22,8 @@ weight = 1005
 ## 解答例 {#解答例}
 
 -   [RScript](https://noboru-murata.github.io/statistical-data-analysis1/code/slide04.R)
+
+
+## 練習用データ {#練習用データ}
+
+-   [jpdata](https://noboru-murata.github.io/statistical-data-analysis1/zips/jpdata.zip)
--- docs/code/slide04.R
+++ docs/code/slide04.R
@@ -1,42 +1,44 @@
-### 例題1
-### データフレームの操作
+### 第04回 練習問題解答例
 
-## 抽出する行番号のベクトルで指定
+### データフレームの操作で用いた例
+
+## 行番号のベクトルで指定して抽出
 airquality[1:10,] # 1-10行を抽出
 
-## 条件に合致する行はTRUE (NAは欠損値)
-airquality[1:16,]$Ozone>100 # 条件の指定
-airquality[1:16,]$Ozone>100 & airquality[1:16,]$Wind<=5 # 条件のAND
-with(airquality[1:16,], Ozone>100 & Wind<=5) # 上と同じ(短い書き方)
-with(airquality[1:24,], Ozone>100 | Wind<=5) # 条件のOR
+## 条件の指定の仕方
+airquality[1:15,]$Ozone>100 # 条件に合致する行はTRUE (NAは欠損値)
+airquality[1:15,]$Ozone>100 & airquality[1:15,]$Wind<=5 # 条件のAND
+with(airquality[1:15,], Ozone>100 & Wind<=5) # 上と同じ(短い書き方)
+with(airquality[1:60,], Ozone>100 | Wind<=5) # 条件のOR
 
-## 関数whichでTRUEの番号を抽出
-which(with(airquality, Ozone>100 & Wind<=5)) # 全データから抽出
+## 関数 which() でTRUEの番号を抽出
+which(with(airquality, Ozone>100 & Wind<=5)) # 全データからTRUEを抽出
 
-## 行の抽出
+## 条件を指定して行を抽出
 airquality[which(with(airquality, Ozone>100 & Wind<=5)), ]
 
-## 列番号のベクトルで指定
+## 列番号のベクトルで指定して抽出
 airquality[which(with(airquality, Ozone>100 & Wind<=5)), c(1,5,6)]
 
 ## 複数の列の場合
 airquality[which(with(airquality, Ozone>100 & Wind<=5)), 
            c("Month","Day")]
 
-## 1つの列の場合は以下でも良い (ただしベクトルになる)
+## 1つの列の場合は以下でも良い (ただしデータフレームではなくベクトルになる)
 airquality[which(with(airquality, Ozone>100 & Wind<=5)),]$Month
+airquality[which(with(airquality, Ozone>100 & Wind<=5)),"Month"] # 上と同じ
+## データフレームとして抽出したい場合は drop=FALSE を指定する
+airquality[which(with(airquality, Ozone>100 & Wind<=5)),"Month",drop=FALSE]
 
-### 例題2
-### 関数subsetの使い方
-
+### 関数 subset() の使い方
 subset(airquality,
        subset = Ozone>100 & Wind<=5,
        select = c(1,5,6))
 subset(airquality,
        Ozone>100 & Wind<=5, # 順序通りなら引数の名前は省略可
-       c(Month,Day)) # 名前は$の後と同じ扱い
+       c(Month,Day)) # 名前は$の後と同じ扱いで "" は不要
 
-## Ozoneに欠測(NA)がなく, かつDayが5か10のWindからDayまでの列を抽出
+## Ozoneに欠測(NA)がなく, かつDayが5か10のデータのWindからDayまでの列を抽出
 subset(airquality, 
        subset = !is.na(Ozone) & Day %in% c(5,10),
        select = Wind:Day)
@@ -46,83 +48,89 @@ subset(airquality,
        subset = Ozone>120 | Wind <= 3,
        select = -Temp)
 
-### 練習1.1
-### 7月のオゾン濃度
+### 練習問題 関数 subset() によるデータフレームの操作
+## 7月のオゾン濃度
 subset(airquality,
        subset = Month == 7,
        select = Ozone)
 
-### 練習1.2
-### 風速時速10マイル以上かつ気温が華氏80度以上
+## 風速時速10マイル以上かつ気温が華氏80度以上
 subset(airquality,
        subset = Wind >= 10 & Temp >=80)
 
-### 練習1.3
-### 日射量が欠測でないデータの月と日
+## 日射量が欠測でないデータの月と日
 subset(airquality,
        subset = !is.na(Ozone) & !is.na(Solar.R),
-       select = c(Month,Day)) # 
+       select = c(Month,Day)) # 書いた順に並ぶ
 subset(airquality, # 書き方はいろいろある
        subset = !(is.na(Ozone) | is.na(Solar.R)),
-       select = Month:Day) # c(Month,Day)と同じ
+       select = Month:Day) # もともと並んでいるので c(Month,Day) と同じ
 
-### 例題3
-### CSVファイルの操作
+### ファイルの取り扱いで用いた例
 
-## 関数write.csvの使い方
+## 関数 write.csv() の使い方 (CSVファイルの操作)
 (myData <- subset(airquality, 
                   subset = Ozone>120,
                   select = -Temp)) # データフレームの作成
 dim(myData) # データフレームの大きさを確認
+## 作業ディレクトリの中に data というフォルダを用意しておく
 write.csv(myData,file="data/mydata.csv") # csvファイルとして書き出し
 
-## 関数read.csvの使い方
-(newdata <- read.csv(file="data/mydata.csv",
+## 関数 read.csv() の使い方 (CSVファイルの操作)
+(newdata <- read.csv(file="data/mydata.csv", # 前の例のファイル
                      row.names=1)) # 1列目を行名に指定
 dim(newdata) # 正しく読み込めたか大きさを確認
 
-### 例題4
-### RDataファイルの操作
-
-## 関数saveの使い方
+### 関数 save() の使い方 (RDataファイルの操作)
 (myDat1 <- subset(airquality, Temp>95, select=-Ozone)) 
 (myDat2 <- subset(airquality, Temp<57, select=-Ozone)) 
 dim(myDat1); dim(myDat2) # 大きさを確認
 save(myDat1,myDat2,file="data/mydata.rdata") # RData形式で書き出し
 
-## 関数loadの使い方
+## 関数 load() の使い方 (RDataファイルの操作)
 (myDat1 <- subset(airquality, Ozone > 160)) # 新たに作成
 load(file="data/mydata.rdata") # RData形式の読み込み
 myDat1 # saveしたときの名前で読み込まれ上書きされる
 myDat2
 
-### 練習2
-### サンプルデータの読み込み
-
+### 練習問題 サンプルデータの読み込み
 myData <- read.csv(file="data/jpdata1.csv", fileEncoding="utf8", row.names=1)
 myItem <- read.csv(file="data/jpdata2.csv", fileEncoding="utf8")
 myArea <- read.csv(file="data/jpdata3.csv", fileEncoding="utf8")
 
 ### データ操作の例
 ## 項目名の内容を確認
-myItem
+myItem 
 ## 最初の6県を表示
-head(myData)
+head(myData) 
 ## 最後の6県を表示
 tail(myData)
 ## 地方名を確認
 myArea
 ## 人口の最大値を見る
 with(myData,max(人口))
-## どの都道府県か調べる
+## どの都道府県の人口多いか調べる
 with(myData,which.max(人口)) # 行番号が返る
 myData[with(myData,which.max(人口)),] 
-## 人口の多い都道府県を調べる
+## 人口の多い順に都道府県を並べる
 with(myData,order(人口,decreasing=TRUE)) # 行番号を取得
 rownames(myData)[with(myData,order(人口,decreasing=TRUE))] # 県名を表示
 
-### 例題5
-### データを集約する関数
+## 上記と同じ内容を英語版で実行する場合
+myDataEn <- read.csv(file="data/jpdata1-en.csv", row.names=1)
+myAreaEn <- read.csv(file="data/jpdata3-en.csv")
+## データを確認する
+head(myDataEn) 
+tail(myDataEn)
+myAreaEn
+## 人口の最大値を見る
+with(myDataEn,max(population))
+## どの都道府県の人口多いか調べる
+myDataEn[with(myDataEn,which.max(population)),] 
+## 人口の多い順に都道府県を並べる
+rownames(myDataEn)[with(myDataEn,order(population,decreasing=TRUE))]
+
+### データの集計で用いた例
 
 myData <- read.csv(file="data/jpdata1.csv",
                    row.names=1, fileEncoding="utf8")
@@ -131,11 +139,18 @@ sum(myData$人口) # 全国の総人口 (列名で選択)
 mean(myData[,4]) # 面積の平均値 (行列として列を選択)
 median(myData[[4]]) # 面積の中央値 (リストとして列を選択)
 min(myData["若年"])  # 若年人口の最小値 (列名で選択)
-with(myData,max(老人))  # 老年人口の最大値 (関数withを利用)
+with(myData,max(老人))  # 老年人口の最大値 (関数 with() を利用)
 
-### 例題6
+### 日本語に不具合がある場合
+myDataEn <- read.csv(file="data/jpdata1-en.csv", row.names=1)
+## 一度読み込んでいれば上の行は不要
+sum(myDataEn$population) # 全国の総人口 (列名で選択)
+mean(myDataEn[,4]) # 面積の平均値 (行列として列を選択)
+median(myDataEn[[4]]) # 面積の中央値 (リストとして列を選択)
+min(myDataEn["young_population"])  # 若年人口の最小値 (列名で選択)
+with(myDataEn,max(old_population))  # 老年人口の最大値 (関数 with() を利用)
+
 ### 関数applyの使い方
-
 x <- subset(myData, select=婚姻:勤女) # 抽出
 colMeans(x) # 各列の平均
 apply(x, 2, max) # 列ごとの最大値
@@ -143,9 +158,14 @@ sapply(x, max)   # 上と同じ (help(sapply)を参照)
 ## 自作関数の適用 (関数に名前を付けずに利用できる)
 apply(x, 2, function(z){sum(z>mean(z))}) # 平均より大きいデータ数
 
-### 例題7
-### 関数aggregateの使い方
+### 日本語に不具合がある場合
+x <- subset(myDataEn, select=marriage:working_hours_female) # 抽出
+colMeans(x) # 各列の平均
+apply(x, 2, max) # 列ごとの最大値
+sapply(x, max)   # 上と同じ (help(sapply)を参照)
+apply(x, 2, function(z){sum(z>mean(z))}) # 平均より大きいデータ数
 
+### 関数aggregateの使い方
 ## 人口から面積まで地方ごとの平均値を計算
 x <- subset(myData,select=人口:面積)
 aggregate(x, by=list(地方=myArea$地方), FUN=mean)
@@ -172,32 +192,57 @@ aggregate( . ~ 地方 + 過疎, FUN=mean, # + で条件を追加
                           地方=myArea$地方,
                           過疎=with(myData, 人口<=median(人口))))
 
-### 練習3.1
+### 日本語に不具合がある場合
+## 人口から面積まで地方ごとの平均値を計算
+x <- subset(myDataEn,select=population:area)
+aggregate(x, by=list(region=myAreaEn$region), FUN=mean) 
+
+aggregate(subset(myDataEn,select=population:area),
+          by=list(region=myAreaEn$region),
+          FUN=mean) 
+
+y <- data.frame(x,region=myAreaEn$region) 
+aggregate( . ~ region, data=y, FUN=mean)
+
+aggregate( . ~ region, # 右辺で条件付けて左辺(右辺以外)を計算
+          data=data.frame(subset(myDataEn,select=population:area),
+                          region=myAreaEn$region), 
+          FUN=mean)
+
+## 地方と，人口が中央値以下か否かでグループ分けして平均値を計算
+aggregate(x, by=list(region=myAreaEn$region,
+                     depop=with(myDataEn, population<=median(population))),
+          FUN=mean)
+
+aggregate( . ~ region + depop, FUN=mean, 
+          data=data.frame(subset(myDataEn,select=population:area),
+                          region=myAreaEn$region,
+                          depop=with(myDataEn, population<=median(population))))
+
+### 練習問題 データの集計
 ### 県別の人口密度 (人口/面積)
 with(myData,人口/面積) # 値のみ返す
 ## 人口密度に関係するデータをまとめてデータフレームを作成
 (myDat1 <- data.frame(subset(myData,select=c(人口,面積)),
-                      人口密度=with(myData,人口/面積)))
+                        人口密度=with(myData,人口/面積)))
 
-### 練習3.2
 ### 地方別の人口密度 (地方の総人口/地方の総面積)
 ## 地方ごとに人口と面積の合計を計算
 (myDat2 <- aggregate(subset(myData,select=c("人口","面積")),
-                     by=list(地方=myArea$地方), FUN=sum))
+                       by=list(地方=myArea$地方), FUN=sum))
 ## 人口密度を計算して追加
 (myDat2 <- data.frame(myDat2, 
                       人口密度=with(myDat2,人口/面積)))
 
-### 練習3.3
 ### 地方別の婚姻率・離婚率
 ## 婚姻可能な人口を推計
 (tmp <- data.frame(subset(myData, select=婚姻:離婚),
                    婚姻可能=with(myData,人口-若年)))
 ## 婚姻率と離婚率から人数を推計
 (tmp <- data.frame(tmp,
-                  婚姻数=with(tmp, 婚姻可能*婚姻/1000),
-                  離婚数=with(tmp, 婚姻可能*離婚/1000),
-                  地方=myArea$地方))
+                   婚姻数=with(tmp, 婚姻可能*婚姻/1000),
+                   離婚数=with(tmp, 婚姻可能*離婚/1000),
+                   地方=myArea$地方))
 ## 地方別の婚姻・離婚数を集計
 (myDat3 <- aggregate(. ~ 地方, FUN=sum,
                      data=subset(tmp,select=-c(婚姻,離婚))))
