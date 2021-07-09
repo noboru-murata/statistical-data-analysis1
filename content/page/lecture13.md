6255f60c6edbbc795bf30286760360979fa9204d
Noboru Murata <noboru.murata@gmail.com>
2021-07-09 14:33:25 +0900

    2021 version

--- content/page/lecture13.md
+++ content/page/lecture13.md
@@ -6,7 +6,7 @@ draft = false
 weight = 1014
 +++
 
-更新中
+<span class="timestamp-wrapper"><span class="timestamp">&lt;2021-07-09 Fri&gt; </span></span> 更新
 
 
 ## スライド {#スライド}
--- docs/code/slide13.R
+++ docs/code/slide13.R
@@ -1,81 +1,103 @@
-### 練習1
-### 回帰モデルの点推定
+### 第13回 練習問題解答例
+
+### 練習問題 回帰モデルの点推定
 ## 気候データによる例
-myData <- read.csv("data/tokyo_weather.csv",
-                   fileEncoding="utf8")
+TW.data <- read.csv("data/tokyo_weather.csv")
 ## データの散布図 (1年分)
 par(family="HiraginoSans-W4") 
-plot(気温 ~ 日射量, data=myData, pch=20, col="blue")
+plot(temp ~ solar, # Y軸 ~ X軸
+     data=TW.data,
+     pch=20, col="blue")
 ## 
-(myModel <- lm(気温 ~ 日射量, data=myData)) # 気温を日射量で説明
+(myModel <- lm(temp ~ solar, # 目的変数 ~ 説明変数
+               data=TW.data)) # 気温を日射量で説明
 ## 回帰直線の図示 (重ね描き)
-abline(reg=myModel, col="red", lwd=2)
+abline(reg=myModel, # 得られた回帰式を用いて描画
+       col="red", lwd=2)
 ## 期間を限って分析する
 ## データの散布図 (夏のモデル)
-plot(気温 ~ 日射量, data=myData, subset=月%in%7:9,
+plot(formula(myModel), # myModel で用いた式を再利用する
+     data=TW.data, subset=month%in%7:9, # 7月-9月
      pch=20, col="orange")
-(myMode2 <- lm(気温 ~ 日射量, data=myData, subset=月%in%7:9)) 
+(myMode2 <- lm(formula(myModel),
+               data=TW.data, subset=month%in%7:9)) 
 abline(reg=myMode2, col="red", lwd=2)
 
-### 練習2
-### 回帰モデルの区間推定
+### 練習問題 回帰モデルの区間推定
 ## 気候データによる例
 ## 前問で構成したモデルを用いる
 ## myModel: 1年分のモデル 
 confint(myModel)
 ## 区間推定を視覚化
-plot(気温 ~ 日射量, data=model.frame(myModel), pch=20, col="blue")
-xrange <- with(myData,range(日射量)) # 日射量の範囲を取得
+plot(formula(myModel),
+     data=model.frame(myModel), # 用いたデータを取り出すことができる
+     pch=20, col="blue")
+xrange <- with(TW.data,range(solar)) # 日射量の範囲を取得
 x <- seq(xrange[1], xrange[2], by=0.5) # 適当な刻み幅で説明変数を用意
-y <- predict(myModel, newdata=data.frame(日射量=x), # 予測値を計算
+y <- predict(myModel,
+             newdata=data.frame(solar=x), # 予測値を計算
              interval="confidence", level=0.95) # 信頼区間を付与
 matlines(x, y, lwd=2,
-         lty=c(1,2,2), col=c("red","pink","pink"))
+         lty=c(1,4,4), col=c("red","pink","pink"))
 ## myMode2: 夏のモデル 
 confint(myMode2)
-plot(気温 ~ 日射量, data=model.frame(myMode2), pch=20, col="orange")
-y <- predict(myMode2, newdata=data.frame(日射量=x),
+plot(formula(myMode2),
+     data=model.frame(myMode2),
+     pch=20, col="orange")
+y <- predict(myMode2,
+             newdata=data.frame(solar=x),
              interval="confidence", level=0.95)
 matlines(x, y, lwd=2,
-         lty=c(1,2,2), col=c("red","pink","pink"))
+         lty=c(1,4,4), col=c("red","pink","pink"))
 
-### 練習3
-### 回帰モデルの係数の検定
+### 練習問題 回帰モデルの係数の検定
 ## 気候データによる例
 ## 前問で構成したモデルを用いる
 ## myModel: 1年分のモデル
 summary(myModel)
 ## 情報が多いので，整理してみる
 summary(myModel)$coef # 名前は識別できれば途中まででも可
-summary(myModel)$fstat
+summary(myModel)$coef["solar",c("t value","Pr(>|t|)")]
+summary(myModel)$coef[2,3:4] # 上と同じ
+summary(myModel)$fstat # モデルの有意性の評価
 ## myMode2: 夏のモデル
 summary(myMode2)
 coef(summary(myMode2)) # 関数coefでも可
+coef(summary(myMode2))["solar",c("t value","Pr(>|t|)")]
+coef(myMode2) # 推定された係数のみ取り出す場合
+coef(summary(myMode2))[,"Estimate"] # 上と同じ
 
-### 練習3
-### 回帰モデルの係数の検定
+### 練習問題 決定係数による回帰モデルの検討
 ## 気候データによる例
 ## 前問で構成したモデルを用いる
 ## myModel: 1年分のモデル (気温~日射量)
+summary(myModel) # 全情報の表示
 summary(myModel)$r.squared
 summary(myModel)$adj.r.squared
 
 ## myMode2: 夏のモデル (気温~日射量)
+summary(myMode2) # 全情報の表示
 summary(myMode2)$r.squared
 summary(myMode2)$adj.r.squared
 
 ## 降水量と気温の関係を調べる
-(myMode3 <- lm(気温 ~ 降水量, data=myData)) 
-(myMode4 <- lm(気温 ~ 降水量, data=myData, subset=月%in%7:9)) 
+(myMode3 <- lm(temp ~ rain, # モデル式
+               data=TW.data)) 
+(myMode4 <- lm(formula(myMode3), # 上の式を用いる
+               data=TW.data, subset=month%in%7:9)) 
 
 ## myModel3: 1年分のモデル (気温~降水量)
-plot(気温 ~ 降水量, data=model.frame(myMode3), pch=20, col="blue")
+plot(formula(myMode3),
+     data=model.frame(myMode3),
+     pch=20, col="blue")
 abline(reg=myMode3, col="red", lwd=2)
 summary(myMode3)
 ## myMode3 に有意性はないことがわかる
 
 ## myMode4: 夏のモデル (気温~降水量)
-plot(気温 ~ 降水量, data=model.frame(myMode4), pch=20, col="orange")
+plot(formula(myMode4),
+     data=model.frame(myMode4),
+     pch=20, col="orange")
 abline(reg=myMode4, col="red", lwd=2)
 summary(myMode4)
 ## 夏場は雨が降ると気温が下がる傾向が有意にあることが読み取れる
