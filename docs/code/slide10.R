3e13206b3ab4c241d293f28aa7740409d470fd28
Noboru Murata <noboru.murata@gmail.com>
2026-04-08 11:39:17 +0900

    2026 version

--- docs/code/slide10.R
+++
@@ -1,7 +0,0 @@
-### 第10講 サンプルコード
-library(tidyverse)
-#' 日本語を用いる場合 macOS では以下の設定を行うと良い
-if(Sys.info()["sysname"] == "Darwin") { # macOS か調べて日本語フォントを指定
-  theme_update(text = element_text(family = "HiraginoSans-W4"))
-  update_geom_defaults("text", list(family = theme_get()$text$family))
-  update_geom_defaults("label", list(family = theme_get()$text$family))}
